fad1d3e490e8bb9c0011c68cc325626c3fb21d5e
CastrilloRedondoVictor <castrilloredondovictor@gmail.com>
2025-03-31 10:21:13 +0200

    Changes added for management

---
+++ ApiProyectoBackPeluqueria/Controllers/ManagementController.cs
@@ -0,0 +1,41 @@
+﻿using ApiProyectoBackPeluqueria.Helpers;
+using ApiProyectoBackPeluqueria.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NugetProyectoBackPeluqueria.Models;
+
+namespace ApiProyectoBackPeluqueria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagementController : ControllerBase
+    {
+
+        private RepositoryPeluqueria repo;
+
+        private HelperActionServicesOAuth helper;
+
+        public ManagementController(RepositoryPeluqueria repo, HelperActionServicesOAuth helper)
+        {
+            this.repo = repo;
+            this.helper = helper;
+        }
+
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult> GetPerfil(int id)
+        {
+            Usuario usuario = await this.repo.FindUsuario(id);
+            return Ok(usuario);
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<ActionResult> UpdateUsuario(Usuario usuario)
+        {
+            await this.repo.UpdateUsuarioAsync(usuario);
+            return Ok();
+        }
+    }
+}
---
+++ ApiProyectoBackPeluqueria/Controllers/ReservasController.cs
@@ -0,0 +1,72 @@
+﻿using ApiProyectoBackPeluqueria.Helpers;
+using ApiProyectoBackPeluqueria.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NugetProyectoBackPeluqueria.Models;
+
+namespace ApiProyectoBackPeluqueria.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReservasController : ControllerBase
+    {
+
+        private RepositoryPeluqueria repo;
+
+        private HelperActionServicesOAuth helper;
+
+        public ReservasController(RepositoryPeluqueria repo, HelperActionServicesOAuth helper)
+        {
+            this.repo = repo;
+            this.helper = helper;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult> FindReserva(int id)
+        {
+            ReservaView reserva = await this.repo.FindReservaAsync(id);
+            return Ok(reserva);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult> GetProximaReservaUsuario(int id)
+        {
+            Reserva reserva = await this.repo.GetProximaReservaUsuarioAsync(id);
+            return Ok(reserva);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult> GetServicioReserva(int id)
+        {
+            string servicio = await this.repo.GetServicioReservaAsync(id);
+            return Ok(servicio);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult> AgregarDisponibilidadRango(DateTime fechaInicio, DateTime fechaFin)
+        {
+            await this.repo.AgregarDisponibilidadRangoAsync(fechaInicio, fechaFin);
+            return Ok();
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult> ObtenerCitasConHoras()
+        {
+            var reservas = await this.repo.ObtenerCitasConHoras();
+            return Ok(reservas);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<ActionResult> ObtenerDiasDisponibles()
+        {
+            var dias = await this.repo.ObtenerDiasDisponibles();
+            return Ok(dias);
+        }
+    }
+}
